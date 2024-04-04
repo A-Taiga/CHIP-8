6b5d16ce1b66900a96f3f3efde5df1b75fd49865
Anthony Polka <anthonypolka@gmail.com>
2024-04-03 21:00:32 -0500

    input

--- src/main.cpp
+++ src/main.cpp
@@ -3,27 +3,12 @@
 #include <cstdlib>
 int main(int argc, char* argv[])
 {
-    // if (argc < 2)
-    // {
-    //     printf("Usage: %s <chip8_ROM>\n\n", argv[0]);
-    //     std::exit(EXIT_SUCCESS);
-    // }
-
-
-    // CPU chip (argv[1]);
-    // CPU chip("roms/roms2/1-chip8-logo.ch8");
-    // CPU chip("roms/roms2/2-ibm-logo.ch8");
-    // CPU chip("roms/roms2/pong.ch8");
-    // CPU chip("roms/roms2/3-corax+.ch8");
-    // CPU chip("roms/roms2/rng.ch8");
-    CPU chip("roms/roms2/6-keypad.ch8");
-    // CPU chip("roms/roms2/5-quirks.ch8");
-    // CPU chip("roms/roms2/Chip8 Picture.ch8");
-    // CPU chip("roms/programs/Life [GV Samways, 1980].ch8");
-    // CPU chip("roms/roms2/4-flags.ch8");
-    // CPU chip("roms/roms2/8-scrolling.ch8");
-
-    // CPU chip(argv[1]);
+    if (argc < 2)
+    {
+        printf("Usage: %s <chip8_ROM>\n\n", argv[0]);
+        std::exit(EXIT_SUCCESS);
+    }
+    CPU chip(argv[1]);
     chip.run();
     return 0;
 }
